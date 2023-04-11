d6aa8d607a5095f1d45b2ce9da0a0c324fc2011a
Anonyo <32406693+anonyosmitra@users.noreply.github.com>
2023-04-11 15:57:14 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='PPY1',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Anonyo,Damian',
+      author_email='s24833@pjwstk.edu.pl'
+      packages=[],
+     )
